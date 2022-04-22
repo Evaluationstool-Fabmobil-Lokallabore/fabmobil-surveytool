be6f455493c8b038c6fe719cf8b2f3aa2b5784d1
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-04-22 16:00:28 +0200

    Add button active states

--- src/components/Fabutton.js
+++ src/components/Fabutton.js
@@ -1,13 +1,34 @@
-function Fabutton({ children, onClick, className, style }) {
-  return (
-    <div
-      onClick={onClick}
-      className={`Fabutton ${className || ""}`}
-      style={style}
-    >
-      {children || "..."}
-    </div>
-  );
+import React from "react";
+import classNames from "classnames";
+
+class Fabutton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+  }
+
+  toggleActiveState() {
+    this.setState({ active: !this.state.active });
+  }
+
+  render() {
+    return (
+      <div
+        onClick={() => {
+          this.toggleActiveState();
+          this.props.onClick();
+        }}
+        className={classNames("Fabutton", this.props.className, {
+          "Fabutton--active": this.state.active,
+        })}
+        style={this.props.style}
+      >
+        {this.props.children || "..."}
+      </div>
+    );
+  }
 }
 
 export default Fabutton;
