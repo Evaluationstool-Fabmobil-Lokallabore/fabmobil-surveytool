16e834f18303d8241745a38dafbd1e9f4152134d
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-04-22 15:45:17 +0200

    Add rough Slider implementation

--- src/components/FabSlider.js
+++ src/components/FabSlider.js
@@ -1,21 +1,56 @@
-function FabSlider({ imgTop, imgBottom, noBottomLabel }) {
-  return (
-    <div className="FabSlider">
-      <div className="FabSlider__elementTop">
-        {imgTop ? <img className="FabSlider__image" src={imgTop} /> : "+"}
+import Slider from "@mui/material/Slider";
+import React from "react";
+
+const DEFAULT_STEP_MIN = 0;
+const DEFAULT_STEP_MAX = 5;
+
+class FabSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
+
+  handleSliderChange(newValue) {
+    this.setState({ value: newValue });
+  }
+
+  render() {
+    return (
+      <div className="FabSlider">
+        <div className="FabSlider__elementTop">
+          {this.props.imgTop ? (
+            <img className="FabSlider__image" src={this.props.imgTop} />
+          ) : (
+            "+"
+          )}
+        </div>
+
+        <div className="FabSlider__bar">
+          <div className="FabSlider__bar__filler"></div>
+          <Slider
+            aria-label="Like"
+            orientation="vertical"
+            min={this.props.minSteps || DEFAULT_STEP_MIN}
+            max={this.props.maxSteps || DEFAULT_STEP_MAX}
+            marks={true}
+            color="secondary"
+            valueLabelDisplay="on"
+            value={this.state.value}
+            onChange={(_, newValue) => this.handleSliderChange(newValue)}
+          />
+        </div>
+        <div className="FabSlider__elementBottom">
+          {this.props.imgBottom ? (
+            <img className="FabSlider__image" src={this.props.imgBottom} />
+          ) : this.props.noBottomLabel ? (
+            ""
+          ) : (
+            "-"
+          )}
+        </div>
       </div>
-      <div className="FabSlider__bar"></div>
-      <div className="FabSlider__elementBottom">
-        {imgBottom ? (
-          <img className="FabSlider__image" src={imgBottom} />
-        ) : noBottomLabel ? (
-          ""
-        ) : (
-          "-"
-        )}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default FabSlider;
