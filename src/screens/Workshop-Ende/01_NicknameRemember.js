3e2b35c9de9a71a894a34866d0b87b7f155458c4
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-05-20 18:14:58 +0200

    Add missing nickname field

--- src/screens/Workshop-Ende/01_NicknameRemember.js
+++ src/screens/Workshop-Ende/01_NicknameRemember.js
@@ -1,4 +1,4 @@
-//import FabTextInput from "../../components/FabTextInput";
+import FabTextInput from "../../components/FabTextInput";
 import WeiterButton from "../../components/WeiterButton";
 import RobiGif from "../../components/RobiGif";
 import robiAuftrittWinken from "../../assets/robi-gifs/Robi_auftritt+winken-min.gif";
@@ -10,6 +10,12 @@ function Screen({ onSubmit, data }) {
         <p>Willkommen zurück!</p>
         <p>Verrätst du mir deinen Nicknamen?</p>
       </div>
+
+      <FabTextInput
+        value={data}
+        onChange={onSubmit}
+        style={{ marginTop: "auto", marginBottom: "auto" }}
+      />
 
       <WeiterButton navigateTo="/wiewars/spass" />
       <RobiGif src={robiAuftrittWinken} style={{ bottom: 0 }} />
