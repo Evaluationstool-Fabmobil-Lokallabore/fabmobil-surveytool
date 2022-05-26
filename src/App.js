2eebcd1a884de1bf01e5681be24d39898e81f9c8
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-05-26 11:08:40 +0200

    Add firebase config

--- src/App.js
+++ src/App.js
@@ -6,18 +6,11 @@ import StartScreen from "./screens/0_Start";
 import SURVEY_WORKSHOPSTART from "./constants/survey-workshopstart.js";
 import SURVEY_WORKSHOPEND from "./constants/survey-workshopend.js";
 import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBCwzI8Q0OpIkrL1bvc78czsw_7pWKAV8w",
-  authDomain: "fabmobil-736e1.firebaseapp.com",
-  projectId: "fabmobil-736e1",
-  storageBucket: "fabmobil-736e1.appspot.com",
-  messagingSenderId: "478835012323",
-  appId: "1:478835012323:web:182ce81d4923831a038bd6",
-};
+import firebaseConfig from "./constants/firebase-config";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app);
 
 const DUMMY_DATABASE = {
   WORKSHOP_START:
