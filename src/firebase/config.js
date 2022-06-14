fa1e21b5fc7970b4f47eed518f69c20110dd7ef2
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-06-14 14:04:03 +0200

    Switch Firebase backend

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 const config = {
-  apiKey: "AIzaSyD-9VwcRvqSH1tOfLac0GADw1i-4XSVfGw",
-  authDomain: "fabmobil-57553.firebaseapp.com",
+  apiKey: "AIzaSyBWh96mSiV4P6k5RpGXBv91SEtyuOOD5fM",
+  authDomain: "fabmobil-survey.firebaseapp.com",
   databaseURL:
-    "https://fabmobil-57553-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fabmobil-57553",
-  storageBucket: "fabmobil-57553.appspot.com",
-  messagingSenderId: "542273828517",
-  appId: "1:542273828517:web:e9bac922bda39ce7e1ff4c",
+    "https://fabmobil-survey-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fabmobil-survey",
+  storageBucket: "fabmobil-survey.appspot.com",
+  messagingSenderId: "507913596848",
+  appId: "1:507913596848:web:827f392c54bfbad100f075",
 };
 
 export default config;
