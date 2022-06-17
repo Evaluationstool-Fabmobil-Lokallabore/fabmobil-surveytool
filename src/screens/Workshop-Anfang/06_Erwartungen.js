59c58803407a3fb04e09d8016cccd8b6c99cd835
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-06-17 16:22:18 +0200

    Fix finger position over text input

--- src/screens/Workshop-Anfang/06_Erwartungen.js
+++ src/screens/Workshop-Anfang/06_Erwartungen.js
@@ -1,21 +1,35 @@
+import { useRef, useState, useEffect } from "react";
 import FabTextArea from "../../components/FabTextArea";
 import WeiterButton from "../../components/WeiterButton";
 import RobiGif from "../../components/RobiGif";
 import robiLongarmtouch from "../../assets/robi-gifs/Robi_longarmtouch-min.gif";
 
 function Screen({ data, onSubmit }) {
+  const containerRef = useRef(null);
+  const [foobar, setFoobar] = useState(0);
+  useEffect(() => {
+    const textBox = containerRef.current.firstElementChild;
+    setFoobar(textBox.offsetTop + textBox.offsetHeight);
+    const resizeListener = () => {
+      setFoobar(containerRef.current.offsetHeight);
+    };
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, [data]);
   return (
     <>
       <div>
         <p>Welche Erwartungen hast du an deine Zeit bei uns im Fabmobil?</p>
       </div>
-      <div className="flex-wrapper">
+      <div className="flex-wrapper" ref={containerRef}>
         <FabTextArea value={data} onChange={onSubmit} />
       </div>
       <WeiterButton navigateTo="/losgehts/interesse" />
       <RobiGif
         src={robiLongarmtouch}
-        style={{ width: 491, left: -123, top: "42%" }}
+        style={{ width: 491, left: -123, top: foobar - 40 }}
       />
     </>
   );
