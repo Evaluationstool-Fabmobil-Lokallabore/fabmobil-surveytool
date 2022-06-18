d27da1aee919656779f53f22df821b072daf74a6
Alexa <5918060+alexabruck@users.noreply.github.com>
2022-06-18 11:56:31 +0200

    Adapt layout with autoexpanding textbox

--- src/components/FabTextArea.js
+++ src/components/FabTextArea.js
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 function FabTextArea({ value, onChange, style, ...otherProps }) {
+  useEffect(() => {
+    //on mount
+    document.getElementsByClassName("Main")[0].classList.add("overflow-scroll");
+    return () => {
+      // Anything in here is fired on component unmount.
+      document
+        .getElementsByClassName("Main")[0]
+        .classList.remove("overflow-scroll");
+    };
+  }, []);
   return (
     <div className="FabTextArea" style={style}>
       <TextareaAutosize
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => {
+          onChange(e.target.value);
+          document.getElementsByClassName("Main")[0].scrollTop =
+            document.getElementsByClassName("Main")[0].scrollHeight;
+        }}
         className="FabTextArea__inner"
         style={{ height: "65px", padding: "10px" }}
         {...otherProps}
