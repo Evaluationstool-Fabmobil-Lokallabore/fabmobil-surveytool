2cf75a260af7b29512472286d3475146f39a891c
alexabruck <5918060+alexabruck@users.noreply.github.com>
2022-10-04 13:07:23 +0200

    Improve pictogram-textlayer styling

--- src/components/ClickablePicto.js
+++ src/components/ClickablePicto.js
@@ -6,12 +6,13 @@ function ClickablePicto({ src, srcActive, title, isActive, style, onClick }) {
       className={classNames("ClickablePicto", {
         "ClickablePicto--active": isActive,
       })}
+      onClick={onClick}
     >
       <img
+        className="ClickablePicto__image"
         style={style}
         title={title}
         alt={title}
-        onClick={onClick}
         src={isActive ? srcActive : src}
       />
       <span className="ClickablePicto__textlayer">{title}</span>
