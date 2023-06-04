eac928639f673d0d590ba043942cf98d276d01e3
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-06-04 16:21:34 +0200

    Convert Obj obj for readers

--- src/helpers/arrToCsv.js
+++ src/helpers/arrToCsv.js
@@ -1,10 +1,23 @@
+function convertToStringable(el) {
+  //convert data objects into something useful for the reader
+  if (Array.isArray(el)) {
+    return `${el}`; //built-in is fine for our case
+  }
+  //if is object
+  if (typeof el === "object") {
+    return JSON.stringify(el);
+  } else return el;
+}
+
 const arrToCsv = (arr, columns, delimiter = ",") =>
   [
     columns.join(delimiter),
     ...arr.map((obj) =>
       columns.reduce(
         (acc, key) =>
-          `${acc}${!acc.length ? "" : delimiter}"${!obj[key] ? "" : obj[key]}"`,
+          `${acc}${!acc.length ? "" : delimiter}"${
+            !obj[key] ? "" : convertToStringable(obj[key])
+          }"`,
         ""
       )
     ),
