44f01e7ebc03bbf1f0d42796249ed75e860f81e1
letnorx <147499884+letnorx@users.noreply.github.com>
2023-10-17 12:13:05 +0200

    update test

--- src/App.dummytest.js
+++ src/App.dummytest.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Workshop Start/i);
   expect(linkElement).toBeInTheDocument();
